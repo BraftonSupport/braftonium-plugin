77a1b9c88fe8974a76ccb1f6a96300205907900c
Yvonne Tse <yvonne.tse@brafton.com>
2018-12-18 15:20:56 -0500

    ?

--- posttypes.php
+++ posttypes.php
@@ -13,8 +13,7 @@ function braftonium_posttypes_init() {
 	global $custom_post_types;
 	if( $custom_post_types ):
 	foreach( $custom_post_types as $custom_post_type ):
-		$custom_post_slug = sanitize_text_field($custom_post_type);
-		$custom_post_santype = ucwords(str_replace('_', ' ', $custom_post_slug));
+		$custom_post_santype = sanitize_text_field(strtolower(str_replace(' ', '-', $custom_post_type)));
 			$posttypes_labels = array(
 				'name'				=> $custom_post_santype,
 				'singular_name'		=> $custom_post_santype,
