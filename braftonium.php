eef306e0812915e23253dd3e220ffa93378d05fc
Deryk w. King <deryk.king@brafton.com>
2020-12-30 15:39:17 -0500

    add resources options

--- braftonium.php
+++ braftonium.php
@@ -167,6 +167,53 @@ if(!function_exists("acf_add_local_field_group")){
 		'active' => 1,
 		'description' => '',
 	));
+	acf_add_local_field_group(array(
+		'key' => 'group_5f91eb67e46c6',
+		'title' => 'Resources Options',
+		'fields' => array(
+			array(
+				'key' => 'field_5f91eb7562df6',
+				'label' => 'Resources Banner',
+				'name' => 'resources_banner',
+				'type' => 'image',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'return_format' => 'array',
+				'preview_size' => 'medium',
+				'library' => 'all',
+				'min_width' => '',
+				'min_height' => '',
+				'min_size' => '',
+				'max_width' => '',
+				'max_height' => '',
+				'max_size' => '',
+				'mime_types' => '',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'options_page',
+					'operator' => '==',
+					'value' => 'braftonium_resources_plugin',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+	));
 }
 
 // make setting page
